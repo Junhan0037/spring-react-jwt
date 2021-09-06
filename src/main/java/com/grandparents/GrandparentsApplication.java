2ad11cc3659041299a6f3ac00df8ec7b6ddf7c42
Junhan0037 <wnsgks0037@naver.com>
2021-09-06 22:16:45 +0900

    feat: Initial commit

---
+++ src/main/java/com/grandparents/GrandparentsApplication.java
@@ -0,0 +1,13 @@
+package com.grandparents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandparentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrandparentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grandparents/GrandparentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandparents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandparentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
