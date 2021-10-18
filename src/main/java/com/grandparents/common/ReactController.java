25eaa0f50e47dea93354c09f7dcd1a5103496dda
Junhan0037 <wnsgks0037@naver.com>
2021-10-18 15:28:30 +0900

    fix: 셀프 조인

--- src/main/java/com/grandparents/common/ReactController.java
+++ src/main/java/com/grandparents/common/ReactController.java
@@ -3,11 +3,12 @@ package com.grandparents.common;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ReactController implements ErrorController {
 
-    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" }, method= {RequestMethod.GET, RequestMethod.POST})
     public String getIndex() {
         return "/index.html";
     }
--- src/main/java/com/grandparents/domain/member/Member.java
+++ src/main/java/com/grandparents/domain/member/Member.java
@@ -4,6 +4,8 @@ import com.grandparents.common.BaseTimeEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter @Builder
@@ -30,5 +32,38 @@ public class Member extends BaseTimeEntity {
     private String address;
 
     private String phone;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member assistant;
+
+    @OneToMany(mappedBy = "assistant", cascade = CascadeType.ALL)
+    private List<Member> members = new ArrayList<>();
+
+    /**
+     * 요양사 변경
+     * @param assistant
+     */
+    public void changeAssistant(Member assistant) {
+        this.setAssistant(assistant);
+        assistant.getMembers().add(this);
+    }
+
+    /**
+     * 회원 추가
+     * @param member
+     */
+    public void addMember(Member member) {
+        this.getMembers().add(member);
+        member.setAssistant(this);
+    }
+
+    /**
+     * 회원 삭제
+     * @param member
+     */
+    public void removeMember(Member member) {
+        this.getMembers().remove(member);
+        member.setAssistant(null);
+    }
 
 }
